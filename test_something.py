c73e6191eff80601be0956dac34858071c85ec2f
ashraful8907 <ashraful8907@gmail.com>
2025-09-29 12:56:13 +0100

    added test and function

---
+++ test_something.py
@@ -0,0 +1,11 @@
+import unittest
+from add_numbers import add_numbers
+
+class TestFunction(unittest.TestCase):
+
+    def test_addition(self):
+        result = add_numbers(4, 6)
+        self.assertEqual(result, 10)
+
+if __name__ == "__main__":
+    unittest.main()
